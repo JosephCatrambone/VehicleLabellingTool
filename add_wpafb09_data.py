32bc7aaf4ebc72a9c023e665a55ce083cf00d20b
Joseph Catrambone <jo.jcat@gmail.com>
2014-06-04 12:46:54 -0400

    Finishing touches.  Bug fixes.

--- add_wpafb09_data.py
+++ add_wpafb09_data.py
@@ -11,19 +11,18 @@ from datetime import datetime
 # id,LATITUDE,LONGITUDE,X,Y,TIME,FRAME_NUMBER,TYPE
 # 1,39.771141447642805,-84.09588282166273,9476,7187,21-OCT-09 08.25.17.087000000 PM,100,M
 
-DB_NAME = 'vehicles.db';
-
 if __name__=="__main__":
 	csv_file = sys.argv[1];
-	db = sqlite3.connect(sys.argv[2] or DB_NAME);
+	db = sqlite3.connect(sys.argv[2]);
 	db.row_factory = sqlite3.Row;
 	cursor = db.cursor();
-	fin = open(csv_file, r);
+	cursor.execute("CREATE TABLE vehicles (id INTEGER PRIMARY KEY, image TEXT, x NUMERIC, y NUMERIC, x_forward NUMERIC, y_forward NUMERIC, submitter TEXT)");
+	fin = open(csv_file, 'r');
 	cin = csv.DictReader(fin);
 	for row in cin:
-		timestamp = datetime.strptime(row['TIME'], "%d-%b-%y %I.%M.%S.%f %p");
+		timestamp = datetime.strptime(row['TIME'], "%d-%b-%y %I.%M.%S.%f000 %p");
 		frame_number = row['FRAME_NUMBER'];
-		image_file = "{}-01000{0:03d}-VIS.ntf.r1.img0_jpg8m.raw.jpg".format(timestamp.strftime('%Y%m%d%H%M%S'), int(frame_number));
+		image_file = "{0}-01000{1:03d}-VIS.ntf.r1.img0_jpg8m.raw.jpg".format(timestamp.strftime('%Y%m%d%H%M%S'), int(frame_number));
 		cursor.execute('INSERT INTO vehicles (image, x, y) VALUES (?, ?, ?)', (image_file, row['X'], row['Y']));
 		print("Inserted ({}, {}, {}) into vehicles.".format(image_file, row['X'], row['Y']));
         db.commit();
